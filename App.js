c62817adeb389026de3c85eb4782d9e20c0b3a6b
LACG <lacisner@pichincha.com>
2020-09-08 22:34:44 -0500

    formik example

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
 
-import { Button, Text, StyleProvider } from 'native-base';
+import { Button, Text, StyleProvider, Item, Input } from 'native-base';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { SvgUri } from 'react-native-svg';
+
+import { Formik } from 'formik';
 
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material';
@@ -19,6 +21,7 @@ import Meme from './meme.svg';
 const NavScreens = {
   Login: 'Login',
   Home: 'Home',
+  From: 'From',
 };
 
 const SafeArea = styled(SafeAreaView)`
@@ -44,7 +47,7 @@ const LoginScreen = ({ navigation: { push } }) => (
   </ScrollContent>
 );
 
-const HomeScreen = () => (
+const HomeScreen = ({ navigation: { push } }) => (
   <Content>
     <Text>Home Screen</Text>
     <SvgUri
@@ -53,6 +56,31 @@ const HomeScreen = () => (
       uri="http://thenewcode.com/assets/images/thumbnails/homer-simpson.svg"
     />
     <Meme width="30%" height="30%" />
+    <Button onPress={() => push(NavScreens.From)}>
+      <Text>Click Me!</Text>
+    </Button>
+  </Content>
+);
+
+const FormScreen = () => (
+  <Content>
+    <Formik initialValues={{ email: '' }} onSubmit={(values) => alert(JSON.stringify(values))}>
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View>
+          <Item regular>
+            <Input
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              placeholder="Enter your email"
+            />
+          </Item>
+          <Button onPress={handleSubmit}>
+            <Text>Submit</Text>
+          </Button>
+        </View>
+      )}
+    </Formik>
   </Content>
 );
 
@@ -68,6 +96,7 @@ const App = () => {
             <Stack.Navigator>
               <Stack.Screen name={NavScreens.Login} component={LoginScreen} />
               <Stack.Screen name={NavScreens.Home} component={HomeScreen} />
+              <Stack.Screen name={NavScreens.From} component={FormScreen} />
             </Stack.Navigator>
           </NavigationContainer>
         </StyleProvider>
